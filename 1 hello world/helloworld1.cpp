6635e9ddf6385946f00a4b8c22084711d8beb6ee
sr-sourabh <sourabhchaudhary012@gmail.com>
2017-04-14 22:28:28 +0530

    added

--- 1 hello world/helloworld1.cpp
+++ 1 hello world/helloworld1.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main()
 {
 	omp_set_num_threads(4);
+	
 
 	#pragma omp parallel
 	{
 	int ID = omp_get_thread_num();
 	cout<<"HELLO"<<ID<<"WORLD"<<ID<<endl;
+	cout<<omp_get_num_threads()<<endl;
 	}
-
+	
 	return 0;
 }
 
--- 2 synchronization/3.c
+++ 2 synchronization/3.c
@@ -12,27 +12,35 @@ int main(void)
 	double  pi;
 	step = 1.0/(double) num_steps; 
 	int nthreads;
-	double sum[NUM]   ;
+	
 	omp_set_num_threads(NUM);
+	
 	
 	#pragma omp parallel
 	{
+		//printf("\n %d \n" , omp_get_num_threads());
+		
 		int nthrds;
 		int i;
+		double sum ;
 		double x;
 		int ID = omp_get_thread_num();
 		nthrds = omp_get_num_threads();
 		if(ID==0)
 			nthreads = nthrds;
 		     
-		for (i=ID,sum[ID] = 0.0;i<num_steps;i += nthrds) 
+		for (i=ID,sum = 0.0;i<num_steps;i += nthrds) 
 		{         
 			x = (i+0.5)*step;         
-			sum[ID] = sum[ID] + 4.0/(1.0+x*x);     
+			sum = sum + 4.0/(1.0+x*x);     
 		}    
+		
+		#pragma omp critical
+			pi += sum * step;
+		
 	    
  	}  
-	for(i = 0,pi=0.0; i<nthreads;i++) pi +=sum[i]*step;    
+	   
 	printf("pi=%f\n",pi);     
 	return EXIT_SUCCESS; 
 }
